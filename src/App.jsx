bb7875404217bc636c5d75a77d9ed317419fccef
CugliariSanti17 <cugliarisantiago@gmail.com>
2024-08-25 17:00:56 -0300

    agrego item list container + prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 const App = () => {
   return (
     <>
       <NavBar />
-
+      <ItemListContainer greeting="Mis productos"/>
     </>
   )
 }
