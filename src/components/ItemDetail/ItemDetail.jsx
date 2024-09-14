7bbd301300695c2ae10fa8d187e1c02954176e94
CugliariSanti17 <cugliarisantiago@gmail.com>
2024-09-14 15:40:13 -0300

    agrego link en icono de detail card + agrego custom hook en detail card

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import "./ItemDetail.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { useContador } from '../../custom-hooks/useContador';
 
-const ItemDetail = ({nombre, precio, descripcion, img}) => {
+const ItemDetail = ({nombre, precio, descripcion, img, stock}) => {
+
+  const {contador, aumentar, disminuir} = useContador(1, stock);
+
   return (
     <div className='cardDetailContainer'>
       <div className='cardDetailImgContainer'>
@@ -13,8 +18,11 @@ const ItemDetail = ({nombre, precio, descripcion, img}) => {
         <h3>{nombre}</h3>
         <p className='itemDescription'>{descripcion}</p>
         <p className='itemPrice'>${precio}</p>
+        <button onClick={disminuir} className='countButton'>-</button>
+        <strong className='countNumber'>{contador}</strong>
+        <button onClick={aumentar} className='countButton'>+</button>
         <button className='itemButton'>Agregar al carrito</button>
-        <FontAwesomeIcon icon={faArrowLeftLong} className='itemIcon' />
+        <Link to={"/"}><FontAwesomeIcon icon={faArrowLeftLong} className='itemIcon'/></Link>
       </div>
     </div>
   )
---
+++ src/custom-hooks/useContador.js
@@ -0,0 +1,19 @@
+import {useState} from "react"
+
+export const useContador = (valorInicial, valorFinal)=>{
+    const [contador, setContador] = useState(valorInicial);
+
+    const aumentar = ()  =>{
+        if(contador < valorFinal){
+            setContador(contador+1);
+        }
+    }
+
+    const disminuir = () =>{
+        if(contador > valorInicial){
+            setContador(contador-1);
+        }
+    }
+
+    return {contador, aumentar, disminuir}
+}
